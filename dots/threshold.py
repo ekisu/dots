68e764aff59fab4111af753594345cec19e8dfbc
ekisu <ramonzitosdesorte@hotmail.com>
2019-04-28 16:59:26 -0300

    threshold: add adaptive_gaussian function

--- dots/threshold.py
+++ dots/threshold.py
@@ -10,10 +10,15 @@ def otsu(matrix):
     _threshold_function, binary_matrix = cv2.threshold(matrix, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     return binary_matrix
 
+def adaptive_gaussian(matrix, block_size=11, c=2):
+    return cv2.adaptiveThreshold(matrix, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, \
+            cv2.THRESH_BINARY, block_size, c)
+
 _THRESHOLD_FUNCTION_MAP = {
     'simple': simple,
     'simple_inv': simple_inv,
-    'otsu': otsu
+    'otsu': otsu,
+    'adaptive_gaussian': adaptive_gaussian
 }
 
 def threshold_function(name):
