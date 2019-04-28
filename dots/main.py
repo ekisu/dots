ddda56418bbe5d636cf868817fd69e184104bd75
ekisu <ramonzitosdesorte@hotmail.com>
2019-04-28 16:59:13 -0300

    main: add flag for passing params to threshold
    some images might be better with some custom parameters, so allow
    the user to fiddle with them.

--- dots/main.py
+++ dots/main.py
@@ -3,13 +3,30 @@ from .threshold import threshold_function
 from .output import output_function
 
 from argparse import ArgumentParser
-from typing import Callable, Any, List
+from typing import Callable, Any, List, Dict, Tuple
 from pathlib import Path
+from ast import literal_eval
+
+def function_args(args_string: str) -> Tuple[List[Any], Dict[str, Any]]:
+    args: List[Any] = []
+    kwargs: Dict[str, Any] = {}
+
+    for arg in args_string.split(','):
+        if arg == '': continue
+        
+        if '=' in arg:
+            [k, v] = arg.split('=')
+            kwargs[k] = literal_eval(v)
+        else:
+            args.append(literal_eval(arg))
+    
+    return args, kwargs
 
 def create_argument_parser() -> ArgumentParser:
     parser = ArgumentParser()
     parser.add_argument("image_path", type=Path)
     parser.add_argument("--threshold", type=threshold_function, default='otsu')
+    parser.add_argument("--threshold-args", type=function_args, default='')
     parser.add_argument("--output", type=output_function, default='braille_3x2')
     parser.add_argument("--resize-factor", type=float, default=1)
     return parser
@@ -17,11 +34,13 @@ def create_argument_parser() -> ArgumentParser:
 def main(image_path: Path,
     resize_factor: float,
     threshold: Callable[[int], Any],
+    threshold_args: Tuple[List[Any], Dict[str, Any]],
     output: Callable[[Any], List[str]]):
     loader = ImageLoader(image_path)
     grayscale_image = loader.as_grayscale()
     resized_image = resize_with_factor(grayscale_image, resize_factor)
-    binary_matrix = threshold(resized_image)
+    t_args, t_kwargs = threshold_args
+    binary_matrix = threshold(resized_image, *t_args, **t_kwargs)
     lines = output(binary_matrix)
 
     print('\n'.join(lines))
