729b1365a5cabeface234d0234394dd46e88b61c
ekisu <ramonzitosdesorte@hotmail.com>
2019-04-28 18:54:11 -0300

    threshold: convert cv2 matrices into boolean ones
    until now, they were just uint8 arrays, with 0 on the false values and 255
    on the true ones.

--- dots/threshold.py
+++ dots/threshold.py
@@ -5,11 +5,11 @@ def simple(matrix, point = 127):
 
 def otsu(matrix):
     _threshold_function, binary_matrix = cv2.threshold(matrix, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    return binary_matrix
+    return binary_matrix == 255
 
 def adaptive_gaussian(matrix, block_size=11, c=2):
     return cv2.adaptiveThreshold(matrix, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, \
-            cv2.THRESH_BINARY, block_size, c)
+            cv2.THRESH_BINARY, block_size, c) == 255
 
 _THRESHOLD_FUNCTION_MAP = {
     'simple': simple,
