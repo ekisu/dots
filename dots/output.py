960dfd7f0db891613ea7f7c692ebe807428f1b6d
ekisu <ramonzitosdesorte@hotmail.com>
2019-04-28 19:15:39 -0300

    output: add braille_4x2 output function

--- dots/output.py
+++ dots/output.py
@@ -1,24 +1,16 @@
-from typing import List
+from typing import List, Dict
 
-def braille_3x2(binary_matrix) -> List[str]:
-    # The Unicode braille character has a pattern, that, from the
-    # base U+2800 (hex), each "dot" can be activated by adding:
-    # 1   8
-    # 2  10    (all those in hex)
-    # 4  20
-    dot_map: List[List[int]] = [
-        [0x1, 0x8],
-        [0x2, 0x10],
-        [0x4, 0x20]
-    ]
-
+def _braille_create_lines(binary_matrix,
+                          y_step: int,
+                          x_step: int,
+                          dot_map: Dict[int, int]) -> List[str]:
     output_lines: List[str] = []
-    for y_pos in range(0, len(binary_matrix), 3):
+    for y_pos in range(0, len(binary_matrix), y_step):
         output_line: str = ""
-        for x_pos in range(0, len(binary_matrix[y_pos]), 2):
+        for x_pos in range(0, len(binary_matrix[y_pos]), x_step):
             offset: int = 0
-            for y_offset in range(3):
-                for x_offset in range(2):
+            for y_offset in range(y_step):
+                for x_offset in range(x_step):
                     try:
                         if binary_matrix[y_pos + y_offset][x_pos + x_offset]:
                             offset += dot_map[y_offset][x_offset]
@@ -29,8 +21,28 @@ def braille_3x2(binary_matrix) -> List[str]:
         output_lines.append(output_line)
     return output_lines
 
+# The Unicode braille character has a pattern, that, from the
+# base U+2800 (hex), each "dot" can be activated by adding:
+# 1   8
+# 2  10    (all those in hex)
+# 4  20
+# 40 80
+_BRAILLE_DOT_MAP: List[List[int]] = [
+    [0x1, 0x8],
+    [0x2, 0x10],
+    [0x4, 0x20],
+    [0x40, 0x80]
+]
+
+def braille_3x2(binary_matrix) -> List[str]:
+    return _braille_create_lines(binary_matrix, 3, 2, _BRAILLE_DOT_MAP)
+
+def braille_4x2(binary_matrix) -> List[str]:
+    return _braille_create_lines(binary_matrix, 4, 2, _BRAILLE_DOT_MAP)
+
 _OUTPUT_FUNCTION_MAP = {
     'braille_3x2': braille_3x2,
+    'braille_4x2': braille_4x2
 }
 
 def output_function(name):
--- tests/test_output.py
+++ tests/test_output.py
@@ -1,5 +1,5 @@
 import unittest
-from dots.output import braille_3x2, output_function
+from dots.output import braille_3x2, braille_4x2, output_function
 import numpy as np
 
 class TestBraille3x2(unittest.TestCase):
@@ -20,6 +20,26 @@ class TestBraille3x2(unittest.TestCase):
         output = braille_3x2(input_matrix)
         self.assertEqual(len(output), 1)
         self.assertEqual(output[0], "⠇")
+
+class TestBraille4x2(unittest.TestCase):
+    def test_single_line(self):
+        input_matrix = np.array([
+            [True, True, True, True]
+        ])
+        output = braille_4x2(input_matrix)
+        self.assertEqual(len(output), 1)
+        self.assertEqual(output[0], "⠉⠉")
+    
+    def test_single_column(self):
+        input_matrix = np.array([
+            [True],
+            [True],
+            [True],
+            [True],
+        ])
+        output = braille_4x2(input_matrix)
+        self.assertEqual(len(output), 1)
+        self.assertEqual(output[0], "⡇")
 
 if __name__ == "__main__":
     unittest.main()
