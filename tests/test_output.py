cdb1d4eaa66bad912398c87bd150ec8568687108
ekisu <ramonzitosdesorte@hotmail.com>
2019-04-28 16:02:03 -0300

    add output tests

---
+++ tests/test_output.py
@@ -0,0 +1,25 @@
+import unittest
+from dots.output import braille_3x2, output_function
+import numpy as np
+
+class TestBraille3x2(unittest.TestCase):
+    def test_single_line(self):
+        input_matrix = np.array([
+            [True, True, True, True]
+        ])
+        output = braille_3x2(input_matrix)
+        self.assertEqual(len(output), 1)
+        self.assertEqual(output[0], "⠉⠉")
+    
+    def test_single_column(self):
+        input_matrix = np.array([
+            [True],
+            [True],
+            [True],
+        ])
+        output = braille_3x2(input_matrix)
+        self.assertEqual(len(output), 1)
+        self.assertEqual(output[0], "⠇")
+
+if __name__ == "__main__":
+    unittest.main()
