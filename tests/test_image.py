585a0258a0461adfa0db198c001a6bd972d696cb
ekisu <ramonzitosdesorte@hotmail.com>
2019-05-03 18:58:25 -0300

    tests: add basic image format load test
    just to see if it works with more than one format

--- tests/test_image.py
+++ tests/test_image.py
@@ -8,6 +8,20 @@ def asset_path_builder(sub_path: Path) -> Path:
     return assets_folder.joinpath(Path("assets"), sub_path)
 
 class TestImageLoader(unittest.TestCase):
+    def test_basic_jpg(self):
+        image = ImageLoader(asset_path_builder("2x1_black_white.jpg"))
+        expected_grayscale = np.array([
+            [0, 255]
+        ], dtype=np.uint8)
+        self.assertTrue((image.as_grayscale() == expected_grayscale).all())
+    
+    def test_basic_png(self):
+        image = ImageLoader(asset_path_builder("2x1_black_white.png"))
+        expected_grayscale = np.array([
+            [0, 255]
+        ], dtype=np.uint8)
+        self.assertTrue((image.as_grayscale() == expected_grayscale).all())
+
     def test_transparency_mask(self):
         image = ImageLoader(asset_path_builder("2x1_transparency_test.png"))
         expected_transparency_mask = np.array([
