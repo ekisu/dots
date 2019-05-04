5da9e65916faeaeee7d297f232b5f36a7985469b
ekisu <ramonzitosdesorte@hotmail.com>
2019-05-04 13:06:52 -0300

    web: add initial convert endpoint
    use some default values, but this probably won't stay as it is now

--- dots/web/routes.py
+++ dots/web/routes.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, request
+from ..image import ImageLoader
+from ..threshold import threshold_function
+from ..output import output_function
 
 bp = Blueprint('routes', __name__)
 
@@ -6,6 +9,13 @@ bp = Blueprint('routes', __name__)
 def index():
     return render_template("index.html")
 
-@bp.route("/convert")
+@bp.route("/convert", methods=["POST"])
 def convert():
-    abort(404)
+    image_file = request.files["image"]
+    image = ImageLoader.from_bytes(image_file.read())
+    threshold_fn = threshold_function("adaptive_gaussian")
+    binary_matrix = threshold_fn(image.as_grayscale())
+    output_fn = output_function("braille_4x2")
+    output = output_fn(binary_matrix)
+    return render_template("convert.html", output=output)
+
