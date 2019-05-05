c38dfa6ae35b27739c1ad6e427de5bd2a92969b9
ekisu <ramonzitosdesorte@hotmail.com>
2019-05-05 15:16:23 -0300

    tests: linting

--- tests/test_image.py
+++ tests/test_image.py
@@ -3,43 +3,53 @@ from dots.image import ImageLoader
 from pathlib import Path
 import numpy as np
 
+
 def asset_path_builder(sub_path: Path) -> Path:
     assets_folder = Path(__file__).parent
     return assets_folder.joinpath(Path("assets"), sub_path)
 
+
 class TestImageLoader(unittest.TestCase):
     def test_inexistant_image(self):
         with self.assertRaises(RuntimeError):
-            image = ImageLoader.from_path(asset_path_builder("inexistant_image.png"))
+            ImageLoader.from_path(asset_path_builder("inexistant_image.png"))
 
     def test_basic_jpg(self):
-        image = ImageLoader.from_path(asset_path_builder("2x1_black_white.jpg"))
+        image = ImageLoader.from_path(
+            asset_path_builder("2x1_black_white.jpg")
+        )
         expected_grayscale = np.array([
             [0, 255]
         ], dtype=np.uint8)
         self.assertTrue((image.as_grayscale() == expected_grayscale).all())
-    
+
     def test_basic_png(self):
-        image = ImageLoader.from_path(asset_path_builder("2x1_black_white.png"))
+        image = ImageLoader.from_path(
+            asset_path_builder("2x1_black_white.png")
+        )
         expected_grayscale = np.array([
             [0, 255]
         ], dtype=np.uint8)
         self.assertTrue((image.as_grayscale() == expected_grayscale).all())
 
     def test_transparency_mask(self):
-        image = ImageLoader.from_path(asset_path_builder("2x1_opaque_transparent.png"))
+        image = ImageLoader.from_path(
+            asset_path_builder("2x1_opaque_transparent.png")
+        )
         expected_transparency_mask = np.array([
             [False, True]
         ])
         transparency_mask = image.transparency_mask()
-        self.assertTrue((transparency_mask == expected_transparency_mask).all())
-    
+        self.assertTrue(
+            (transparency_mask == expected_transparency_mask).all()
+        )
+
     def test_from_bytes(self):
         # PPM file
         ppm_data_black_white = (
-            b"P3\n" # PPM header
-            b"2 1\n" # 2x1
-            b"255\n" # Maximum value
+            b"P3\n"   # PPM header
+            b"2 1\n"  # 2x1
+            b"255\n"  # Maximum value
             b"0 0 0 255 255 255\n"
         )
         image = ImageLoader.from_bytes(ppm_data_black_white)
@@ -51,6 +61,7 @@ class TestImageLoader(unittest.TestCase):
     def test_empty_from_bytes(self):
         with self.assertRaises(RuntimeError):
             ImageLoader.from_bytes(b"")
+
 
 if __name__ == "__main__":
     unittest.main()
--- tests/test_threshold.py
+++ tests/test_threshold.py
@@ -2,21 +2,22 @@ import unittest
 from dots.threshold import simple, otsu, adaptive_gaussian, threshold_function
 import numpy as np
 
+
 class TestSimpleThreshold(unittest.TestCase):
     def test_basic(self):
         image = np.array([[0, 127, 128, 255]], dtype=np.uint8)
-        output = simple(image, point = 127)
+        output = simple(image, point=127)
         self.assertTrue((output == [
             [False, False, True, True]
         ]).all())
-    
+
     def test_different_point(self):
         image = np.array([[0, 127, 128, 255]], dtype=np.uint8)
-        output = simple(image, point = 128)
+        output = simple(image, point=128)
         self.assertTrue((output == [
             [False, False, False, True]
         ]).all())
-    
+
     def test_via_threshold_function(self):
         image = np.array([[0, 127, 128, 255]], dtype=np.uint8)
         threshold_fn = threshold_function("simple")
@@ -25,30 +26,34 @@ class TestSimpleThreshold(unittest.TestCase):
             [False, False, True, True]
         ]).all())
 
+
 class TestOtsuThreshold(unittest.TestCase):
     def test_basic(self):
         image = np.array([[0, 127, 255]], dtype=np.uint8)
         output = otsu(image)
         # The middle point value doesn't really matter...
-        self.assertFalse(output[0][0] == True)
-        self.assertTrue(output[0][2] == True)
-    
+        # Also we need to ignore the linter errors because
+        # the inner numpy bool type can't be compared with "is".
+        self.assertFalse(output[0][0] == True)  # noqa: E712
+        self.assertTrue(output[0][2] == True)   # noqa: E712
+
     def test_peaks(self):
         image = np.array([
             [125, 125, 126, 126, 127, 127, 127, 128, 128, 128],
             [250, 251, 252, 253, 254, 254, 255, 255, 255, 255]
         ], dtype=np.uint8)
         output = otsu(image)
-        self.assertTrue((output[0] == False).all())
-        self.assertTrue((output[1] == True).all())
-    
+        self.assertTrue((output[0] == False).all())  # noqa: E712
+        self.assertTrue((output[1] == True).all())   # noqa: E712
+
     def test_via_threshold_function(self):
         image = np.array([[0, 127, 255]], dtype=np.uint8)
         threshold_fn = threshold_function("otsu")
         output = threshold_fn(image)
         # The middle point value doesn't really matter...
-        self.assertFalse(output[0][0] == True)
-        self.assertTrue(output[0][2] == True)
+        self.assertFalse(output[0][0] == True)  # noqa: E712
+        self.assertTrue(output[0][2] == True)   # noqa: E712
+
 
 class TestAdaptiveGaussianThreshold(unittest.TestCase):
     def test_basic(self):
@@ -57,7 +62,7 @@ class TestAdaptiveGaussianThreshold(unittest.TestCase):
         self.assertTrue((output == [
             [False, True]
         ]).all())
-    
+
     def test_via_threshold_function(self):
         image = np.array([[0, 255]], dtype=np.uint8)
         threshold_fn = threshold_function("adaptive_gaussian")
@@ -65,6 +70,7 @@ class TestAdaptiveGaussianThreshold(unittest.TestCase):
         self.assertTrue((output == [
             [False, True]
         ]).all())
+
 
 if __name__ == "__main__":
     unittest.main()
